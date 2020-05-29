a16182582483da2e5d2950120cd946dbbdd70f25
8Bts <mcmizze@yahoo.com>
2020-05-29 12:38:40 +0400

    Added my_all?, my_any?, my_none?  methods

--- main.rb
+++ main.rb
@@ -1,40 +1,71 @@
 module Enumerable
 
-    def my_each
-      self.size.times do | counter |
-        if block_given?
-          yield(self[counter])
-          self
-        else to_enum(__method__)
-        end     
-      end  
+  def my_each
+    self.size.times do | counter |
+      if block_given?
+        yield(self[counter])
+        self
+      else to_enum(__method__)
+      end     
+    end  
+  end
+
+  def my_each_with_index
+    self.size.times do | counter |
+      if block_given?
+        yield(self[counter], counter)
+        self
+      else to_enum(__method__)
+      end     
+    end  
+  end
+
+  def my_select
+    arr = []
+    if block_given?
+      self.my_each do | elem |   
+        arr << elem if yield(elem)
+      end
+    else to_enum(__method__)
     end
-  
-    def my_each_with_index
-      self.size.times do | counter |
-        if block_given?
-          yield(self[counter], counter)
-          self
-        else to_enum(__method__)
-        end     
-      end  
+    arr  
+  end
+
+  def my_all?(pattern = nil)
+    if block_given?
+      self.my_each { |elem| return false unless yield(elem) }
+    elsif pattern
+      self.my_each { |elem| return false unless pattern === elem } 
+    else
+      self.my_each { |elem| return false unless elem }
     end
 
-    def my_select
-        arr = []
-        if block_given?
-          self.my_each do | elem |   
-            arr << elem if yield(elem)
-          end
-        else to_enum(__method__)
-        end
-        arr  
-      end
+    true
+
+  end
+    
+  def my_any?(pattern = nil)
+    if block_given?
+        self.my_each { |elem| return true if yield(elem) }
+    elsif pattern
+        self.my_each { |elem| return true if pattern === elem } 
+    else
+        self.my_each { |elem| return true if elem }
+    end
+
+    false
+
+  end
+
+  def my_none?(pattern = nil)
+    return !self.my_any?(pattern) if (block_given? == false && pattern)
+    !self.my_any? { |elem| block_given?? yield(elem): elem }
+  end  
   
 end
 
 def multiply_els(arr)
-    #arr.my_inject(1, :*)
-    arr.my_inject { |acc, elem| acc*elem }
+  #arr.my_inject(1, :*)
+  arr.my_inject { |acc, elem| acc*elem }
 end
   
